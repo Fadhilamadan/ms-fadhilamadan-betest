3049891974e41548209e9b2c31fec92b7f4f2ad7
Fadhil Amadan <fadhil.amadan@mitrais.com>
2024-08-21 06:26:51 +0700

    refactor(route): improve get last login with jwt

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -4,10 +4,11 @@ import {
   getAccountByLastLogin,
   login,
 } from "src/controllers/account.controller";
+import { authenticateJWT } from "src/middlewares/auth.middlewares";
 
 const router = Router();
 
 router.post("/login", login);
-router.get("/lastLogin", getAccountByLastLogin);
+router.get("/lastLogin", authenticateJWT, getAccountByLastLogin);
 
 export default router;
