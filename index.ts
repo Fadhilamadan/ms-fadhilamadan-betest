8e9a6a131edb36161490c20087073588c6c3aadc
Fadhil Amadan <fadhil.amadan@mitrais.com>
2024-08-20 16:42:12 +0700

    refactor(app): improve if url not found

--- index.ts
+++ index.ts
@@ -15,6 +15,10 @@ mongoose.connect(process.env.MONGO_URI!);
 
 app.use("/api/", apiRouter);
 
+app.all("*", function (req, res) {
+  return res.status(400).send("Build with ❤ by Fadhil Amadan");
+});
+
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}/api`),
 );
